9c464dd4710f95d3d3d994e83986268d26d67aa7
卢乐颜 <frosen_lu@126.com>
2017-04-15 17:28:56 +0800

    xxxx lua

--- xxx.lua
+++ xxx.lua
@@ -4,21 +4,7 @@ lua
 没有do while 和 repeat until ，被 repeat end 取代
 没有if then，被 if do 取代
 没有 .. ，被 + 取代
-
-5.0
-不能用_G, 每个文件，在load时，就是在执行一个function，这时会有个本地环境，被叫做 self的table，所有文件内的不带local的写法都是self这个table的属性
-其他的function在创建时只要不是local的都会存在本地环境指向拥有它的table
-如：a = 1 为 self.a = 1
-可用此实现面向对象
-:不再可用，因为面向对象用 环境 了，而且:也容易用错，
-						
-function中，用<xxx>来引用upvalue，只能引用上一层的
-local a = 1
-function () <a>
-    a = a + 1
-end
-可以避免self与upvalue的冲突，也提高了查找upvalue的效率
-						
+ 
 统一 for 的用法为 for xx = xxx do end 
 原来的 for k,v in pairs(table_name) do
 	print(k,v)
@@ -33,8 +19,90 @@ for i = range(1, 10) do
 	print(i)
 end
 
+5.0
+不能用_G, 环境的概念改变，每个非local的function都有一个名为self的环境表，指向调用它的table，
+一个文件也是一个function，所以指向本文件的self
+可实现面向对象
+:不再可用，因为面向对象用 环境 了，而且:也容易用错，
+移除setEnv等，新增全局函数 call，call(func, env table, arg1, ...), 同时pcall，xpcall都增加了env的参数
+如果function改变了它所在的table，则环境也改变了
+
+为了避免冲突，upvalue需要用$指定，如
+local a = 0
+local function add(n)
+	$a = $a + n
+	return $a
+end
+
+require() 会返回这个文件的环境，也就是说文件底部不再用return
+
 5.1
-其他一些全局函数尽可能变成类函数，
+一些全局函数尽可能变成类函数，
 如 unpack, setmetatable 为 table.unpack, table.setmetatable
 
-没有关键字 in while until then 没有符号 : .. 没有特殊字符 _G 增加特殊字符 this range 增加全局函数bind 增加符号<>，self意义改变
+没有关键字 in while until then 没有符号 : .. 没有特殊字符 _G 增加特殊字符 range 修改 self
+
+-----------------------
+
+local Class = require "class"
+local Node = require "CC.Node"
+
+s = {}
+s.Size = require "CC.Size"
+s.Color = require "CC.Color"
+
+Class.class(self, "newLayer", Node)
+
+-- Class.class(self, "UserData", "BaseData")
+
+local MIN_C = 1
+local MAX_C = 5
+
+function ctor(r_color_colorTab, g, b)
+	call(super.ctor, self)
+
+	size = s.Size.new(0, 0)
+	color = nil
+	otherColors = {}
+
+	local t = type(r_color_colorTab)
+
+	if t == "number" do
+		color = s.Color.new(r_color_colorTab, g, b)
+	elseif t == "color" do
+		color = r_color_colorTab
+	elseif t == "table" do
+		color = r_color_colorTab[1]
+		otherColors = r_color_colorTab
+	end 	
+end
+
+function setColor(c)
+	color = c
+end
+
+function getMixColor()
+	if #otherColors <= 1 do
+		return color
+	end
+
+	local mix = s.Color.clear
+	for _, color = ipairs(otherColors) do
+		for i = range($MIN_C, $MAX_C) do
+			mix = s.Color.mix(color, mix) 
+		end
+	end
+
+	return mix
+end
+
+
+
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
