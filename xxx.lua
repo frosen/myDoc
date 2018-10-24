031adc1919538f06abf83e30ce550f6ace276c39
frosen <frosen_lu@126.com>
2018-10-24 15:12:46 +0800

    Update xxx.lua

--- xxx.lua
+++ xxx.lua
@@ -50,7 +50,7 @@ require，命令行调用也同样，第一个值也是参数，如 reuqire("xxx
 math库全部为number的元表
 原来的全局函数仍可以用，如 math.floor(1) 和 (1).floor() ，因为math是number的元表，通过元表的index方法将self赋值到math的参数上
 
-tostring() 可以把table转换成字符串，便于和 loadstring 一起高效进行序列化和反序列化
+tableserialize(table, k) 可以把table转换成字符串，便于和 loadstring 一起高效进行序列化和反序列化，k为级数默认无限
 
 没有关键字 in while until then 没有符号 : .. 没有特殊字符 _G 增加特殊字符 range % 修改 self
 
